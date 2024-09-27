2e23b2264b57dd84859bd510b0c5ba8f5aa9620f
El Mostafa El Bahtouri <elel-bah@e1r10p3.1337.ma>
2024-09-27 16:21:57 +0100

    fix the atoi for the overflow and make the filder philo

--- philo/utils.c
+++ philo/utils.c
@@ -22,33 +22,83 @@ int	report_error(char *str)
 	return (1);
 }
 
-int	ft_atoi_custom(const char *nptr)
+// New function to check for overflow
+int check_overflow(long long n, int digit, int sign)
 {
-	int			i;
-	int			ti;
-	int			sign;
-	long long	n;
-
-	i = 0;
-	sign = 1;
-	n = 0;
-	if (nptr[i] == '-')
-		sign *= -1;
-	if (nptr[i] == '-' || nptr[i] == '+')
-		i++;
-	ti = i - 1;
-	while (nptr[++ti])
-	{
-		if (nptr[ti] < '0' || nptr[ti] > '9')
-			return (-1);
-	}
-	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		n = n * 10 + (nptr[i] - '0');
-		i++;
-	}
-	return (n * sign);
+    if (n > INT_MAX / 10 || (n == INT_MAX / 10 && digit > INT_MAX % 10))
+    {
+        if (sign == 1)
+            return -1;
+        else if (sign == -1 && n > -(long long)INT_MIN / 10)
+            return 0;
+        else if (sign == -1 && n == -(long long)INT_MIN / 10 && digit > -(INT_MIN % 10))
+            return 0;
+    }
+    return 1;
 }
+
+// Modified ft_atoi_custom function
+int ft_atoi_custom(const char *nptr)
+{
+    int i;
+    long long n;
+    int sign;
+
+    i = 0;
+    n = 0;
+    sign = 1;
+    while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+        i++;
+    if (nptr[i] == '-' || nptr[i] == '+')
+    {
+        if (nptr[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (nptr[i] >= '0' && nptr[i] <= '9')
+    {
+        int overflow = check_overflow(n, nptr[i] - '0', sign);
+        if (overflow != 1)
+            return overflow;
+        n = n * 10 + (nptr[i] - '0');
+        i++;
+    }
+    while (nptr[i])
+    {
+        if (nptr[i] != ' ' && (nptr[i] < 9 || nptr[i] > 13))
+            return -1;
+        i++;
+    }
+    return (int)(n * sign);
+}
+
+// int	ft_atoi_custom(const char *nptr)
+// {
+// 	int			i;
+// 	int			ti;
+// 	int			sign;
+// 	long long	n;
+
+// 	i = 0;
+// 	sign = 1;
+// 	n = 0;
+// 	if (nptr[i] == '-')
+// 		sign *= -1;
+// 	if (nptr[i] == '-' || nptr[i] == '+')
+// 		i++;
+// 	ti = i - 1;
+// 	while (nptr[++ti])
+// 	{
+// 		if (nptr[ti] < '0' || nptr[ti] > '9')
+// 			return (-1);
+// 	}
+// 	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
+// 	{
+// 		n = n * 10 + (nptr[i] - '0');
+// 		i++;
+// 	}
+// 	return (n * sign);
+// }
 
 long long	get_current_time(void)
 {
